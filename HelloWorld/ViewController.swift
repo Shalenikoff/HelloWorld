51dcdedbebad4a910ee85a0154287810497d90f5
Kirill Shalenikov <KShalen@gmail.com>
2023-01-07 15:19:38 +0300

    Реализовал логику приложения и отработку всех элементов

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingButton: UIButton!
+    @IBOutlet var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        greetingLabel.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10
     }
 
 
+    @IBAction func greetingButtonDidTapped() {
+        greetingLabel.isHidden.toggle()
+        
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+       
+    }
 }
 
